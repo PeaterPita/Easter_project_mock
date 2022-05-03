6864592db824e6187e8d814316c93619ca129034
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2022-05-03 23:40:14 +0100

    added TODOS

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ class Client:
     def __init__(self):
 
 
-
-
+        # Initialize the root window
 
         self.root = tkinter.Tk()
         self.root.title("Casino")
@@ -34,15 +33,21 @@ class Client:
         self.root.configure(background=BACKGROUND_COLOR)
 
 
+    # Function to get the users profile pic. If the original file is not found, it will return the default profile pic.
+
     def getProfilePic(self):
         try:
             profilePic = Image.open(self.userData['ProfilePicture'])
         except:
             print("Something went wrong when trying to get the profile picture. Restorted back to default")
             profilePic = Image.open("images/default.jpg")
+
         pic = profilePic.resize((75,75), Image.ANTIALIAS)
         pic = ImageTk.PhotoImage(pic)
         return pic
+
+
+    # Function to validate wether the users password is acceptable. Used in create account and change password.
 
     def validatePassword(self, password):
         if len(password) < 6:
@@ -55,40 +60,57 @@ class Client:
             return (True, )
 
 
+    # Main Func to save all data for user. Can update self.userData anywhere but to save data to db must call this func.
+
     def saveChanges(self):
         with open('data\mydata.json', 'r+') as f:
             data = json.load(f)
             for i, _ in enumerate(data):
                 if _['UserID'] == self.userData['UserID']:
                     data[i] = self.userData
+
+
+            # Reset pointer to beginning of file after reading. As well as truncating it incase data inputted is smaller than original.
+
             f.seek(0)
             json.dump(data, f, indent=4)
             f.truncate()
 
 
+            # Func to update all labels that have old data. Not very important if it fails, wont break anything just wont have uptodate names and pics.
+
             try:
+
+
+                # Try to update username text on main menu
+
                 self.userNameLabel.config(text=f'Welcome, \n {self.userData["username"] }' )
 
 
-                # Try to change man menu profile pic.
+                # Try to cupdate profile pic on main menu
 
                 pic = self.getProfilePic()
                 self.profilePic.config(image=pic)
                 self.profilePic.image = pic
             
+
             except Exception as e:
                 print(e)
 
 
-    
+            # Allow the settings button to be pressed again.
+
+            self.profilePic.config(state='normal')
+
+            
+    # Main warning function. Used to warn the user of any errors. Only need to supply parent and message
+
     def warning(self, parent, message, bg='red', fg='#fff', fontsize=8, row=5, sticky='ew'):
             warn = tkinter.Label(parent, text=message, bg=bg, fg=fg,border=5, relief="raised", font=('Helvetica', fontsize), padx=10, pady=10)
             warn.grid(row=row, column=0, sticky=sticky, padx=20, columnspan=2)
 
 
-
-
-    # Closing Manager
+    # Closing Manager For root
 
     def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
@@ -100,6 +122,11 @@ class Client:
     def settings(self):
 
 
+        # Disable settings button so multiple windows can't be opened at once. Idiot proofing.
+
+        self.profilePic.config(state='disabled')
+
+
         # Creating the settings Modal
 
         self.settingsModal = tkinter.Toplevel(self.root)
@@ -109,12 +136,26 @@ class Client:
         self.settingsModal.configure(background=BACKGROUND_COLOR)
         self.settingsModal.wm_attributes("-topmost", 0)
 
+
+        # Allows user to change profile picture by selecting a new picture from their local disk. 
+
         def changePic():
             newPic  = askopenfilename(initialdir="images", title="Select Profile Picture", filetypes=(("png files", "*.png"), ("all files", "*.*")))
             self.userData['ProfilePicture'] = newPic
             self.saveChanges()
-        
 
+
+        # If user discards changes, warn them and get confirmation.
+        
+        def discard():
+            if messagebox.askokcancel("Close settings", "Are you sure you want to close settings without saving? Your changes will not be saved"):
+                self.settingsModal.destroy()
+                self.profilePic.config(state='normal')
+
+
+        # bind discard to the 'X' button on the top right of the window.
+
+        self.settingsModal.protocol("WM_DELETE_WINDOW", lambda: discard())
 
 
         # Func to find what widget has focus, and set it apart if its a text box.
@@ -148,11 +189,10 @@ class Client:
         currentUsernameEntry.insert(0, self.userData['username'])
 
 
+        # Creating and positioning the button to select new profile picture
+
         profilePicSelector = tkinter.Button(cosmeticFrame, text="Change Profile Picture", bg=BACKGROUND_COLOR, fg='#fff', command= lambda: changePic())
         profilePicSelector.grid(row=1, column=0, sticky='w', padx=10, pady=10)
-
-
-
 
 
         # Creating and positiiing the confrim and new password labels and entries 
@@ -175,21 +215,24 @@ class Client:
         saveChanges = tkinter.Button(self.settingsModal, text="Save Changes", bg=BACKGROUND_COLOR, fg='#fff', command= lambda: updateData())
         saveChanges.grid(row=6, column=0, sticky='news', pady=10, padx=20)
 
-        discardChanges = tkinter.Button(self.settingsModal, text="Discard Changes", bg=BACKGROUND_COLOR, fg='#fff', command= lambda: self.settingsModal.destroy())
+        discardChanges = tkinter.Button(self.settingsModal, text="Discard Changes", bg=BACKGROUND_COLOR, fg='#fff', command= lambda: discard())
         discardChanges.grid(row=6, column=1, sticky='news', pady=10, padx=20)
 
 
-
-        
+        # Func to update the data in the userData dictionary, and save it to the userData.json file. aswell as close settings. 
 
         def updateData():
-        
             self.userData['username'] = currentUsernameEntry.get()
+
+
+            # This is only here because i messed up the below if statmnent tree, bodge fix.
 
             if not confirmPasswordEntry.get() and not newPasswordEntry.get():
                 self.saveChanges()
                 self.settingsModal.destroy()
 
+
+            # If the user has entered their old password but not a new one, warn the user to do so - also check for the opposite. in either case return and dont update anything.
 
             if confirmPasswordEntry.get() and not newPasswordEntry.get():
                 self.warning(self.settingsModal, "Please enter a new password")
@@ -197,6 +240,9 @@ class Client:
             elif newPasswordEntry.get() and not confirmPasswordEntry.get():
                 self.warning(self.settingsModal, "You must confirm your password before changing it")
                 return
+
+
+            # If the user has entered both their old and new password, check if what they entered is actually their password - if not, warn them and return. If it is validate the password is usable.
 
             else:
                 if confirmPasswordEntry.get() == self.userData['password']:
@@ -211,19 +257,10 @@ class Client:
                     return
 
 
-
-
-
+            # If password passes all checks save the changes and close the settings window.
 
             self.saveChanges()
             self.settingsModal.destroy()
-
-
-
-
-
-
-
 
 
         # settingsModal row and col config
@@ -235,8 +272,6 @@ class Client:
         # Binding the focus event to mouse click.
 
         self.settingsModal.bind_all("<Button-1>", lambda event: focus(event))
-
-
 
 
     # Auth Manages the login and registration of the user as well as the modal.
@@ -268,10 +303,6 @@ class Client:
         self.logInModal.protocol("WM_DELETE_WINDOW", self.on_closing)
 
 
-
-        
-
-        
         # Func to find what widget has focus, and set it apart if its a text box.
 
         def focus(event):
@@ -413,6 +444,8 @@ class Client:
         # Function to switch back to the login screen.
        
         def switch_to_login():
+
+
             # Change title and size of the modal.
 
             self.logInModal.title("Log In")
@@ -439,7 +472,12 @@ class Client:
     # Function to create the main menu.
 
     def mainMenu(self):
+
+
+        # Call getProfilePic for later
+
         pic = self.getProfilePic()
+
         
         # Creation and placement of the sidebar frame.
 
@@ -469,70 +507,40 @@ class Client:
         self.sideBarFrame.rowconfigure(3, weight=1)
 
 
+        # Creation and placement of the users name at the top of the sidebar
+
         self.userNameLabel = tkinter.Label(self.sideBarFrame, text=f'Welcome, \n{self.userData["username"]}',font=("Helvetica", 12), bg=BACKGROUND_COLOR, fg='#fff')
         self.userNameLabel.grid(row=0, column=0, sticky='new', columnspan=3,pady=20, padx=20)
 
+
+        # Creation and placement of the profile picture at the top of the sidebar
 
         self.profilePic = tkinter.Button(self.sideBarFrame ,image=pic, command=lambda: self.settings(), pady=10,)
         self.profilePic.image = pic
         self.profilePic.grid(row=1, column=1, sticky='n',)
 
 
+        # Creation and placement of the Users current chip counter under profile picture.
 
         self.chipCounter = tkinter.Label(self.sideBarFrame, text=f'Chips: {self.userData["balance"]}', font=("Helvetica", 12))
         self.chipCounter.grid(row=2, column=0, padx=20, columnspan=3, sticky='new', pady=10)
 
         
+        # Creation and placement a tips section. 
+        # TODO: Might change later. leaderboard? Recent games? not sure. just didnt like all the white space.
+
         TipsFrame = tkinter.LabelFrame(self.sideBarFrame, text="Tips", labelanchor="nw", bd=4, font=("Helvetica", 12))
         TipsFrame.grid(row=3, column=0, columnspan=3, sticky='news', pady=10, padx=20)
 
         
-
-
-
-
-
-
-
-
-
         # Quit Button Creation and placement
 
         QuitButton = tkinter.Button(self.sideBarFrame, text="Quit", command=self.on_closing, )
         QuitButton.grid(row=4, column=0, sticky='sew', padx=10, pady=10, columnspan=3)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # Debug option so i know what profile im working with. 
+        #FIXME : Remove this later.
 
         self.root.bind('<Return>', lambda event: print(self.userData))
 
@@ -540,13 +548,16 @@ class Client:
     # Func to start client. Starts auth first.
 
     def run(self):
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-        if not DEBUG:
-            self.auth()
-        else:
-            self.userData = {'username': 'testing', 'password': 'test', "balance": 999, "ProfilePicture": "images/MMLogo2.png"}
-            self.mainMenu()
 
+
+        # Bind custome close manager to root window
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        
+
+        # Start auth process and mainloop func.
+
+        self.auth()
         self.root.mainloop()
 
 
@@ -558,3 +569,34 @@ class Client:
 
 h = Client()
 h.run()
+
+
+
+###TODOS###############################################################################################################################################################
+
+#TODO: make settings actually look good. Placements are a bit weird atm
+#TODO: Allowing for more types of images for pics
+#TODO: Limit allowed profile pics to min size. 100x100?
+#TODO: Figure out what to put in sidebar white space - Tips? Leaderboard?
+#TODO: Look into multiplayer support. - Game is already kinda set up for it. Just need server and porting
+#TODO: Actually make the casino games
+#TODO: Make game selection menu - Could do list of buttons or one button that changes. - Look at old casino for how to do that!
+#TODO: Proper dev debug menu
+#TODO: Xp and Levling system. - wayy further done could unlock.. things?
+#TODO: Maybe figure out better way to update db specif items. Very slow atm with lots of seperate instances of opening
+#TODO: Look into if python can connect to mongoDB
+#TODO: Make copy of userProfilePic in /images/ incase pic is deleted from local disk
+#TODO: Sort out all the fonts and sizes - some buttons still clipping
+#TODO: Find a suitable theme - do like the dark gray and white atm. Not very casino looking tho.
+#TODO: When changing username add the same check as when making a new account.
+#TODO: Fix entry focusing issues - Some boxes left highlighted after focus is lost.
+#TODO: Make it more obvious that settings is accessed by clikcing on profile pic. - Tips Section on first boot?
+#TODO: Maybe increase size of main menu. Sidebar is bigger than expected
+#TODO: Distinguish quit button from the background. Blends in with the sidebar
+#TODO: Fix multiple settings windows being allowed to be open after changing profile pic? Odd behavior. - not sure why this is
+#TODO: Find something to fill extra space in settings modal. Or decrease size of modal.
+#TODO: Find why username label flickers after clicking back from modal
+#TODO: Find an actaul good looking font for general
+#TODO: Test if program looks the same on other platforms. - Tkinter uses native components i think
+#TODO: Fix focus trying to change the foreground of widgets that dont have a foreground option
+#TODO: 
